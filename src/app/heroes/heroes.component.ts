d2ee3c042491426f9247bea3717a09ebfd71f238
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-17 14:07:48 +0100

    cambios

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
-import { Hero } from '../app/hero';
-import { HEROES } from '../app/mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent {
-
-  heroes = HEROES;
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
